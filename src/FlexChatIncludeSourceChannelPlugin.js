6b808c8f919afb3b88caac038e91519069f06d7b
Lindsay Hamoudi <lhamoudi@twilio.com>
2021-07-08 15:40:02 -0700

    Added logic to show notification if trying to accept a task that's for a chat channel that's already in use in another task (e.g. in wrapup)

--- src/FlexChatIncludeSourceChannelPlugin.js
+++ src/FlexChatIncludeSourceChannelPlugin.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 import { FlexPlugin } from "flex-plugin";
-import { Tab, TaskHelper } from "@twilio/flex-ui";
+import { Notifications, NotificationType, Tab, TaskHelper } from "@twilio/flex-ui";
 import SourceChatChannelCanvas from "./components/SourceChatChannelCanvas";
 
 import SourceChatChannelNotification from "./components/SourceChatChannelNotification";
 
 const PLUGIN_NAME = "FlexChatIncludeSourceChannelPlugin";
+const CHAT_CHANNEL_OPEN_IN_ANOTHER_TASK = 'ChatChannelOpenInAnotherTask';
 
 export default class FlexChatIncludeSourceChannelPlugin extends FlexPlugin {
+  
   constructor() {
     super(PLUGIN_NAME);
   }
@@ -21,7 +23,46 @@ export default class FlexChatIncludeSourceChannelPlugin extends FlexPlugin {
    * @param manager { import('@twilio/flex-ui').Manager }
    */
   init(flex, manager) {
+  
+    manager.strings[CHAT_CHANNEL_OPEN_IN_ANOTHER_TASK] = (
+      'There is already a task open between this customer and you. Please wrapup the existing task before accepting another'
+    );
 
+    Notifications.registerNotification({
+      id: CHAT_CHANNEL_OPEN_IN_ANOTHER_TASK,
+      closeButton: true,
+      content: CHAT_CHANNEL_OPEN_IN_ANOTHER_TASK,
+      type: NotificationType.warning,
+      timeout: 10000
+    });
+
+    flex.Actions.addListener("beforeAcceptTask", (payload, abortFunction) => {
+      const { task } = payload;
+      // Loop through current tasks and make sure there are none open with same channel SID
+      // Can happen in long-lived scenario - e.g. if agent Ends Chat, but is still wrapping when another task 
+      // arrives for the same long-lived chat channel
+      // WE ABSOLUTELY DO NOT WANT AGENT TO BE JOINED TO SAME CHANNEL TWICE IN FLEX - OTHERWISE WHEN ONE TASK IS
+      // WRAPPED UP, THE AGENT WILL LOSE ALL MESSAGES IN THE OTHER TASK(S) FOR THE SAME CHANNEL
+      if (TaskHelper.isChatBasedTask(task)) {
+        const array = Array.from(manager.store.getState().flex.worker.tasks.values());
+        console.log(`Existing tasks: ${array}`)
+        const channelSid = TaskHelper.getTaskChatChannelSid(task);
+        let result = array.find((potentialTask) => 
+            channelSid === TaskHelper.getTaskChatChannelSid(potentialTask) &&
+            task.sid !== potentialTask.sid && 
+            TaskHelper.isTaskAccepted(potentialTask)
+        );
+
+        if (result) {
+          Notifications.showNotification(CHAT_CHANNEL_OPEN_IN_ANOTHER_TASK);
+          console.log("CHAT ALREADY OPEN! " + result.sid);
+          abortFunction();
+        } else {
+          console.log("NO CHAT FOUND!");
+
+        }
+      }
+    });
 
     flex.TaskCanvas.Content.add(
       <SourceChatChannelNotification 
